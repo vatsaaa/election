54cd56d1f87cc9d09835c799e4eb8c1c44c8f339
Ankur Vatsa <ankur.vatsa@gmail.com>
2022-02-05 22:32:38 +0530

    #3: Fully implemented contract and tests.

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -9,12 +9,20 @@ contract Election {
         uint votes;
     }
 
+    // Store addresses that have voted
+    mapping(address => bool) public voters;
+
     // Store Candidates
     // Fetch Candidate
     mapping(uint => Candidate) public candidates; // Cannot be iterated, and has no way to determine the size
 
     // Store Candidates count
     uint public candidateCount;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
     
     // Constructor
     constructor () {
@@ -22,10 +30,26 @@ contract Election {
         addCandidate("Candidate 2");
     }
 
-    // No one else but the contract can add the candidates
-    // Hence, make it private
+    // No one else but the contract can add the candidates, make the function private
     function addCandidate(string memory _name) private {
         candidateCount++;
         candidates[candidateCount] = Candidate(candidateCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        // Check if the sender has already voted
+        require(!voters[msg.sender]);
+
+        // Require a valid candidate is voted for
+        require(_candidateId > 0 && _candidateId <= candidateCount);
+
+        // record that sender has voted
+        voters[msg.sender] = true;
+
+        // Increment the candidate's vote count
+        candidates[_candidateId].votes++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
